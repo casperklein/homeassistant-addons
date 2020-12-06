a70d60b69f525b8d87995b3072557698ea424c7f
casperklein <casperklein@users.noreply.github.com>
2020-12-06 23:48:09 +0100

    build improved

--- netbox/build.sh
+++ netbox/build.sh
@@ -2,9 +2,9 @@
 
 set -ueo pipefail
 
-USER=$(grep USER= Dockerfile | cut -d'"' -f2)
-NAME=$(grep NAME= Dockerfile | cut -d'"' -f2)
-VERSION=$(grep VERSION= Dockerfile | cut -d'"' -f2)
+USER=$(grep -P 'ENV\s+USER=".+?"' Dockerfile | cut -d'"' -f2)
+NAME=$(grep -P 'ENV\s+NAME=".+?"' Dockerfile | cut -d'"' -f2)
+VERSION=$(grep -P 'ENV\s+VERSION=".+?"' Dockerfile | cut -d'"' -f2)
 TAG="$USER/$NAME:$VERSION"
 
 DIR=${0%/*}
@@ -13,3 +13,4 @@ cd "$DIR"
 echo "Building: $NAME $VERSION"
 echo
 docker build -t "$TAG" .
+docker tag "$TAG" "$USER/$NAME:latest"
