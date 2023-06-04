0b00ed8eaef2433183e550ee69881332772bb574
casperklein <casperklein@users.noreply.github.com>
2023-06-04 03:36:48 +0200

    update build scripts

--- netbox/build-addon.sh
+++ netbox/build-addon.sh
@@ -2,17 +2,17 @@
 
 set -ueo pipefail
 
+DIR=${0%/*}
+cd "$DIR"
+
 VERSION=$(jq -er '.version'		< config.json)
 IMAGE=$(jq -er '.image'			< config.json)
 TAG=$(jq -er '"\(.image):\(.version)"'	< config.json)
-
-DIR=${0%/*}
-cd "$DIR"
 
 ARCH=$(dpkg --print-architecture)
 [ "$ARCH" == "arm64" ] && ARCH="aarch64"
 
 echo "Building: $TAG"
 echo
-docker build -t "$TAG" --build-arg VERSION="$VERSION" --build-arg BUILD_ARCH="$ARCH" .
+docker build -t "$TAG" --build-arg VERSION="$VERSION" --build-arg BUILD_ARCH="$ARCH" --provenance=false .
 docker tag "$TAG" "$IMAGE:latest"
