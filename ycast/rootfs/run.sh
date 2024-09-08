d1700d0e2b8875c872e80906fd4ca4a6b24e992c
casperklein <casperklein@users.noreply.github.com>
2024-09-08 16:33:54 +0200

    small improvements

--- ycast/rootfs/run.sh
+++ ycast/rootfs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BOOKMARKS=$(jq --raw-output '.bookmarks | .[]' /data/options.json | sed 's/^/  /')
+BOOKMARKS=$(jq --raw-output '.bookmarks | .[]' /data/options.json 2> /dev/null | sed 's/^/  /')
 
 if [ -n "$BOOKMARKS" ]; then
 	echo "Bookmarks:"  > stations.yml
@@ -10,6 +10,6 @@ fi
 cat stations.yml
 echo " "
 
-export FLASK_ENV=development
+export FLASK_ENV=production # obsolet? https://stackoverflow.com/a/52162979/568737
 export FLASK_DEBUG=0
 exec /usr/bin/python3 -m ycast -d
