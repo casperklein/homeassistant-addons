264346d3ee3d0251ec20983f28042512946fa712
casperklein <casperklein@users.noreply.github.com>
2025-04-06 21:47:48 +0200

    remove legacy code, remove broken https support, add gunicorn, add nginx

--- netbox/rootfs/run.sh
+++ netbox/rootfs/run.sh
@@ -62,31 +62,32 @@ if [ ! -f "$NETBOX_SECRET_KEY" ]; then
 fi
 
 if [ ! -d /data/postgresql/15 ]; then
-	_info "Moving database to persistant storage.."
+	_info "Moving database to persistent storage.."
 	mkdir -p /data/postgresql/15
 	mv /var/lib/postgresql/15/main /data/postgresql/15
 fi
 # Change PostgreSQL data directory
-_info "Configuring PostgreSQL.."
-sedfile -i "s;^data_directory.*;data_directory = '/data/postgresql/15/main';" /etc/postgresql/15/main/postgresql.conf
+if [ ! -f /first_start ]; then
+	_info "Configuring PostgreSQL.."
+	sedfile -i "s;^data_directory.*;data_directory = '/data/postgresql/15/main';" /etc/postgresql/15/main/postgresql.conf
+fi
 
-# Make media files persistant
+# Make media files persistent
 if [ ! -d /data/media ]; then
-	_info "Moving media to persistant storage.."
+	_info "Moving media to persistent storage.."
 	mv /opt/netbox/netbox/media /data/
 fi
-_info "Setting up media usage.."
-rm -rf /opt/netbox/netbox/media
-ln -s /data/media /opt/netbox/netbox/media
+
+# Is already symlink? --> netbox restarted (backup hooks)
+if [ ! -h /opt/netbox/netbox/media ]; then
+	_info "Setting up media usage.."
+	rm -rf /opt/netbox/netbox/media
+	ln -s /data/media /opt/netbox/netbox/media
+fi
 
 # Get user/pass from Home Assistant options
 USER=$(jq --raw-output '.user' /data/options.json)
 PASS=$(jq --raw-output '.password' /data/options.json)
-
-# Get HTTPS settings
-HTTPS=$(jq --raw-output '.https' /data/options.json)
-CERT=$(jq --raw-output '.certfile' /data/options.json)
-KEY=$(jq --raw-output '.keyfile' /data/options.json)
 
 # Create /config/configuration-merged.py ?
 DEBUG=$(jq --raw-output '.debug' /data/options.json)
@@ -107,48 +108,40 @@ fi
 # remove stale pid
 rm /data/postgresql/15/main/postmaster.pid 2>/dev/null && _info "Removing stale PostgreSQL pid file.."
 
-# set netbox option
-if [ "$LOGIN_REQUIRED" = false ]; then
-	# https://docs.netbox.dev/en/stable/configuration/security/#login_required
-	_info "Setting 'LOGIN_REQUIRED' to 'false' in configuration.py"
-	sedfile -i 's/^LOGIN_REQUIRED = True$/LOGIN_REQUIRED = False/' "$NETBOX_CONFIG"
-fi
+if [ ! -f /first_start ]; then
+	# set netbox option
+	if [ "$LOGIN_REQUIRED" = false ]; then
+		# https://docs.netbox.dev/en/stable/configuration/security/#login_required
+		_info "Setting 'LOGIN_REQUIRED' to 'false' in configuration.py"
+		sedfile -i 's/^LOGIN_REQUIRED = True$/LOGIN_REQUIRED = False/' "$NETBOX_CONFIG"
+	fi
 
-echo -e "\n# custom configuration starts here" >> "$NETBOX_CONFIG"
+	echo -e "\n# custom configuration starts here" >> "$NETBOX_CONFIG"
 
-# update secret key
-_info "Restoring secret key.."
-cat "$NETBOX_SECRET_KEY" >> "$NETBOX_CONFIG"
+	# update secret key
+	_info "Restoring secret key.."
+	cat "$NETBOX_SECRET_KEY" >> "$NETBOX_CONFIG"
 
-# import additional configuration (e.g. for plugins)
-if [ -f "/homeassistant/netbox/configuration.py" ]; then
-	# TODO kept for compatibility: 2024-02-24 / netbox 3.7.3
-	_info "Custom configuration (config/netbox/configuration.py) found."
-	cat /homeassistant/netbox/configuration.py >> "$NETBOX_CONFIG"
-fi
-if [ -f "$NETBOX_CONFIG_CUSTOM" ]; then
-	_info "Custom configuration (addon_configs/0da538cf_netbox/configration.py) found."
-	dos2unix -q "$NETBOX_CONFIG_CUSTOM"
-	cat "$NETBOX_CONFIG_CUSTOM" >> "$NETBOX_CONFIG"
-fi
+	# import additional configuration (e.g. for plugins)
+	if [ -f "$NETBOX_CONFIG_CUSTOM" ]; then
+		_info "Custom configuration (addon_configs/0da538cf_netbox/configration.py) found."
+		dos2unix -q "$NETBOX_CONFIG_CUSTOM"
+		cat "$NETBOX_CONFIG_CUSTOM" >> "$NETBOX_CONFIG"
+	fi
 
-# import additional requirements (e.g. for plugins)
-if [ -f "/homeassistant/netbox/requirements.txt" ]; then
-	# TODO kept for compatibility: 2024-02-24 / netbox 3.7.3
-	_info "Installing custom requirements (config/netbox/requirements).."
-	uv pip install --no-cache-dir -r /homeassistant/netbox/requirements.txt
-fi
-if [ -f "/config/requirements.txt" ]; then
-	dos2unix -q /config/requirements.txt
-	_info "Installing custom requirements (addon_configs/0da538cf_netbox/requirements.txt).."
-	uv pip install --no-cache-dir -r /config/requirements.txt
-fi
+	# import additional requirements (e.g. for plugins)
+	if [ -f "/config/requirements.txt" ]; then
+		dos2unix -q /config/requirements.txt
+		_info "Installing custom requirements (addon_configs/0da538cf_netbox/requirements.txt).."
+		uv pip install --no-cache-dir -r /config/requirements.txt
+	fi
 
-if [ "$DEBUG" = true ]; then
-	_debug "Creating /config/configuration-merged.py for debugging.."
-	echo    "# $(date +%c)"                                           > /config/configuration-merged.py
-	echo -e "# This file is auto-generated and just for debugging\n" >> /config/configuration-merged.py
-	cat "$NETBOX_CONFIG"                                             >> /config/configuration-merged.py
+	if [ "$DEBUG" = true ]; then
+		_debug "Creating /config/configuration-merged.py for debugging.."
+		echo    "# $(date +%c)"                                           > /config/configuration-merged.py
+		echo -e "# This file is auto-generated and just for debugging\n" >> /config/configuration-merged.py
+		cat "$NETBOX_CONFIG"                                             >> /config/configuration-merged.py
+	fi
 fi
 
 _info "Starting redis.."
@@ -167,59 +160,49 @@ while ! pg_isready -q; do
 done
 _info "PostgreSQL is ready.."
 
-MANAGE_PY="python3 /opt/netbox/netbox/manage.py"
+_info "Starting nginx.."
+supervisorctl start nginx > /dev/null
+while ! netstat -nlpt | grep -qP '0\.0\.0\.0:80.+LISTEN.+nginx'; do
+	_info "Waiting until nginx is ready.."
+	sleep 1
+done
+_info "Nginx is ready.."
 
-# run migration when needed
-_info "Check if migration is needed.."
-if ! $MANAGE_PY migrate --check &>/dev/null; then
-	netbox-upgrade.sh
-else
-	# Run collectstatic, regardless of whether the database needs migration (https://github.com/casperklein/homeassistant-addons/issues/28)
-	$MANAGE_PY collectstatic --no-input
+if [ ! -f /first_start ]; then
+	MANAGE_PY="python3 /opt/netbox/netbox/manage.py"
+
+	# run migration when needed
+	_info "Check if migration is needed.."
+	if ! $MANAGE_PY migrate --check &>/dev/null; then
+		netbox-upgrade.sh
+	else
+		# Run collectstatic, regardless of whether the database needs migration (https://github.com/casperklein/homeassistant-addons/issues/28)
+		$MANAGE_PY collectstatic --no-input
+	fi
+
+	# add netbox superuser
+	if [ -n "$USER" ] && [ -n "$PASS" ]; then
+		_info "Creating new netbox superuser: $USER"
+		# Create user + API token: https://github.com/netbox-community/netbox-docker/blob/f1ca9ab7ebc16b288fd9da8825176c75d6b7ea4f/docker/docker-entrypoint.sh#L74-L80
+		if ! RESULT=$($MANAGE_PY shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$USER', '${MAIL:-}','$PASS')" 2>&1); then
+			if grep -qP 'Key \(username\)=.+ already exists' <<<"$RESULT"; then
+				_error "Failed to create superuser '$USER', because the user already exists. Remove 'user' and 'password' from the add-on options."
+			else
+				_error "$RESULT"
+				_error "Failed to create superuser '$USER'. See error details above."
+			fi
+			_info " The add-on will continue startup in 1 minute."
+			sleep 1m
+			# exit 1
+		fi >&2
+	fi
 fi
 
-# add netbox superuser
-if [ -n "$USER" ] && [ -n "$PASS" ]; then
-	_info "Creating new netbox superuser: $USER"
-	# Create user + API token: https://github.com/netbox-community/netbox-docker/blob/f1ca9ab7ebc16b288fd9da8825176c75d6b7ea4f/docker/docker-entrypoint.sh#L74-L80
-	if ! RESULT=$($MANAGE_PY shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$USER', '${MAIL:-}','$PASS')" 2>&1); then
-		if grep -qP 'Key \(username\)=.+ already exists' <<<"$RESULT"; then
-			_error "Failed to create superuser '$USER', because the user already exists. Remove 'user' and 'password' from the add-on options."
-		else
-			_error "$RESULT"
-			_error "Failed to create superuser '$USER'. See error details above."
-		fi
-		_info " The add-on will continue startup in 1 minute."
-		sleep 1m
-		# exit 1
-	fi >&2
-fi
-
-# enable HTTPS
-if [ "$HTTPS" = true ]; then
-	_info "Enabling HTTPS.."
-	[ ! -f "/ssl/$CERT" ] && _error "Certificate '$CERT' not found." >&2 && exit 1
-	[ ! -f "/ssl/$KEY" ] && _error "Certificate key '$KEY' not found." >&2 && exit 1
-
-	cat > /etc/stunnel/stunnel.conf <<-"CONFIG"
-		pid = /var/run/stunnel.pid
-		[https]
-		accept  = 80
-		connect = 443
-		cert = /etc/stunnel/stunnel.pem
-	CONFIG
-
-	cat /ssl/{"$CERT","$KEY"} > /etc/stunnel/stunnel.pem
-	chmod 400 /etc/stunnel/stunnel.pem
-
-	/etc/init.d/stunnel4 start || {
-		_error "Failed to start stunnel SSL encryption wrapper."
-		exit 1
-	} >&2
-	PORT=443
-else
-	PORT=80
-fi
+# 'first_start' checks are not needed anymore, switched to "cold" backup. Keeping for the future, where I might give this another chance.
+# For unknown reason, this config fails: "backup_pre": "supervisorctl stop nginx && supervisorctl stop housekeeping && supervisorctl stop rqworker && supervisorctl stop netbox && supervisorctl stop redis && supervisorctl stop postgresql"
+# HA Supervisor is not really helpful and just complains generally about "Pre-/Post backup command returned error code: 1".
+# Running the 'backup_pre' command manually in the container works fine (exit code 0)
+touch /first_start
 
 # Housekeeping
 _info "Starting housekeeping background job.."
@@ -230,10 +213,12 @@ _info "Starting RQ worker process.."
 supervisorctl start rqworker > /dev/null
 
 _info "Starting netbox.."
-# exec gunicorn --bind 127.0.0.1:$PORT --pid /var/tmp/netbox.pid --pythonpath /opt/netbox/netbox --config /opt/netbox/gunicorn.py netbox.wsgi
 
 [ "$DEBUG" = true ] && _debug "Startup took $SECONDS seconds."
 
-$MANAGE_PY runserver 0.0.0.0:$PORT --insecure &
-NETBOX_PID=$!
-wait
+# $MANAGE_PY runserver 0.0.0.0:80 --insecure &
+# NETBOX_PID=$!
+# wait
+
+# Listen on 127.0.0.1:8001
+exec gunicorn --pid /var/tmp/netbox.pid --pythonpath /opt/netbox/netbox --config /opt/netbox/gunicorn.py netbox.wsgi
